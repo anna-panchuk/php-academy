84190390ee47424c345b292e08f55c0414158cbb
Anna Panchuk <anna-m66@yandex.ru>
2016-10-17 22:18:52 +0300

    batleship

---
+++ practise/lessons_4/js/batleship.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+var isSunk = false,
+    count = 0,
+    positiveCount = 0,
+    cell1 = 5,
+    cell2 = 6,
+    cell3 = 7,
+    userInput,
+    stat,
+    SHIP_SIZE = 3;
+
+while (isSunk == false) {
+    userInput = parseInt(prompt("Номер ячейки"));
+    if (userInput == null || isNaN(userInput) || userInput < 1 || userInput > 7) {
+        continue;
+    }
+    count++;
+    if (userInput == cell1 || userInput == cell2 || userInput == cell3) {
+        positiveCount++;
+        alert("Вы попали");
+        if (positiveCount == SHIP_SIZE) {
+            isSunk = true;
+        }
+    } else {
+        alert("Мазило!");
+    }
+}
+alert("Вы потопили корабль");
+stat = SHIP_SIZE * 100 / count;
+alert("Процент попаданий: " + stat + "%");
